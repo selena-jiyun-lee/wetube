87e4c5d04bca28eb1900d2b1ee17c92d0ca0d5cf
Jiyun Lee <selena.jy.lee@gmail.com>
2020-09-17 15:52:14 +0100

    Study Express Middlewares

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("WELCOME");
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send("Welcome to home");
+
+const handleProfile = (req, res) => res.send("This is profile");
+
+app.use(cookieParser()); // record user info
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet()); // for security
+app.use(morgan("dev")); // record log
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
