f75cb1466fad32670ac5ccc1a1a9048505d6e2a0
Jiyun Lee <selena.jy.lee@gmail.com>
2020-10-06 18:32:58 +0100

    Add try/catch to videoDetail

--- controllers/videoControllers.js
+++ controllers/videoControllers.js
@@ -44,14 +44,18 @@ export const postUpload = async (req, res) => {
 	res.redirect(routes.videoDetail(newVideo.id));
 };
 export const videoDetail = async (req, res) => {
-	const {
-		params: { id }
-	} = req;
-	const video = await Video.findById(id);
-	res.render("videoDetail", {
-		pageTitle: "Video Detail",
-		video
-	});
+	try {
+		const {
+			params: { id }
+		} = req;
+		const video = await Video.findById(id);
+		res.render("videoDetail", {
+			pageTitle: "Video Detail",
+			video
+		});
+	} catch (error) {
+		res.redirect(routes.home);
+	}
 };
 export const editVideo = (req, res) =>
 	res.render("editVideo", {
